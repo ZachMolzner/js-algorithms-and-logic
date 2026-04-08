6c8a28a628ccc1f4654608f193749a4fdde570e1
Zach Molzner <ZAMolzner@gmail.com>
2026-04-08 15:45:46 +0000

    index.js

---
+++ src/index.js
@@ -0,0 +1,9 @@
+// arrays
+export * from "./arrays/some-function.js";
+
+// objects
+export * from "./objects/frequency-counter.js";
+export * from "./objects/group-by-property.js";
+
+// recursion
+export * from "./recursion/some-recursive-function.js";
